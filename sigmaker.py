2509939378b6b502d22677f089b3726284cfdaaa
Athanasios Kostopoulos <athanasios@timescale.com>
2023-07-03 18:23:49 +0200

    added sanity checks

--- sigmaker.py
+++ sigmaker.py
@@ -24,14 +24,14 @@ except ModuleNotFoundError:
 
 lock = mp.Lock()
 
-def safe_print(msg, l):
+def safe_print(msg, l) -> None:
     l.acquire()
     try:
         print(msg)
     finally:
         l.release()
 
-def somewhat_safe_serialize(obj, fname, l):
+def somewhat_safe_serialize(obj, fname, l) -> None:
     l.acquire()
     try:
         with open(fname, "wb") as f:
@@ -39,7 +39,7 @@ def somewhat_safe_serialize(obj, fname, l):
     finally:
         l.release()
 
-def parse_directory(dname, postfix):
+def parse_directory(dname, postfix) -> list:
     results = []
     with os.scandir(dname) as it:
         for entry in it:
@@ -48,6 +48,40 @@ def parse_directory(dname, postfix):
             elif entry.is_dir():
                 parse_directory(os.path.join(dname,entry.name), postfix)
     return results
+
+
+def check_output_dir(odir) -> None:
+    if os.path.exists(odir):
+        print(f"{odir} already exists! Aborting!")
+        sys.exit(-4)
+    try:
+        os.mkdir(odir)
+    except PermissionError as e:
+        print(e)
+        sys.exit(-5)
+    except FileNotFoundError as err:
+        print(err)
+        sys.exit(-6)
+
+def check_libtype(ltype) -> None:
+    if ltype.endswith(".a") or ltype.endswith("dylib")
+        return
+    else:
+        print(f"{ltype}: unsupported library type! Aborting!")
+        sys.exit(-10)
+
+
+def check_scan_dir(sdir) -> None:
+    if sdir is None:
+        print(f"{sdir} is None? Aborting!")
+        sys.exit(-7)
+    if not os.path.exists(sdir):
+        print(f"{sdir} does not exist! Aborting!")
+        sys.exit(-8)
+    if not os.path.isdir(sdir):
+        print(f"{sdir} is not a directory! Aborting!")
+        sys.exit(-9)
+
 
 def parse_lib(dylib) -> list:
     func_info = {}
@@ -66,19 +100,6 @@ def parse_lib(dylib) -> list:
     return results
 
 
-def check_output_dir(odir) -> None:
-    if os.path.exists(odir):
-        print(f"{odir} already exists! Aborting!")
-        sys.exit(-4)
-    try:
-        os.mkdir(odir)
-    except PermissionError as e:
-        print(e)
-        sys.exit(-5)
-    except FileNotFoundError as err:
-        print(err)
-        sys.exit(-6)
-
 def main() -> int:
     flock = mp.Lock()
     parser = argparse.ArgumentParser()
@@ -89,7 +110,9 @@ def main() -> int:
     outdir = args.output
     scandir = args.scan
     libtype = args.libtype
+    check_scan_dir(scandir)
     check_output_dir(outdir)
+    check_libtype(libtype)
     libs = []
     pool = mp.Pool(mp.cpu_count())
     for path, directories, files in os.walk(scandir):
